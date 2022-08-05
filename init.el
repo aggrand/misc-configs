eaade3a83506766773e461864fe2504c6bc01800
Carl Raiden Worley <carl.worley@10gen.com>
2022-08-05 17:20:31 -0400

    stop the littering backups

--- init.el
+++ init.el
@@ -1,44 +1,41 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("d2e0c53dbc47b35815315fae5f352afd2c56fa8e69752090990563200daae434" default))
- '(initial-frame-alist '((fullscreen . maximized)))
- '(org-export-backends '(ascii html icalendar latex md odt))
- '(package-selected-packages
-   '(org-tempo visual-fill-column org-bullets forge evil-magit magit projectile hydra general ivy-rich rainbow-delimiters markdown-mode evil-collection ivy-prescient prescient doom-modeline yaml-mode counsel ivy which-key darktooth-theme key-chord evil))
- '(safe-local-variable-values
-   '((projectile-project-name . "data-pipes")
-     (projectile-project-name . "signal-processing-service"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+  (package-initialize)
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(custom-safe-themes
+     '("d2e0c53dbc47b35815315fae5f352afd2c56fa8e69752090990563200daae434" default))
+   '(initial-frame-alist '((fullscreen . maximized)))
+   '(org-export-backends '(ascii html icalendar latex md odt))
+   '(package-selected-packages
+     '(org-tempo visual-fill-column org-bullets forge evil-magit magit projectile hydra general ivy-rich rainbow-delimiters markdown-mode evil-collection ivy-prescient prescient doom-modeline yaml-mode counsel ivy which-key darktooth-theme key-chord evil)))
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   )
 
-(require 'use-package)
-(setq use-package-always-ensure t)
+  (require 'use-package)
+  (setq use-package-always-ensure t)
 
-(use-package which-key
-  :config
-  (which-key-mode))
+  (use-package which-key
+    :config
+    (which-key-mode))
 
-;; TODO: Set up more keys!
-(use-package general
-  :config
-  (general-create-definer crw/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC"))
+  ;; TODO: Set up more keys!
+  (use-package general
+    :config
+    (general-create-definer crw/leader-keys
+      :keymaps '(normal insert visual emacs)
+      :prefix "SPC"
+      :global-prefix "C-SPC"))
 
-(use-package hydra)
+  (use-package hydra)
 
 ;; font size
 (set-face-attribute 'default nil :font "Hack" :height 220)
@@ -97,46 +94,46 @@
   :config
   (ivy-prescient-mode 1))
 
-;; evil mode muahaha
-(use-package evil
-  :init
-  (setq evil-want-minibuffer t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-integration t)
-  :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join))
+    ;; evil mode muahaha
+    (use-package evil
+      :init
+      (setq evil-want-minibuffer t)
+      (setq evil-want-keybinding nil)
+      (setq evil-want-integration t)
+      :config
+      (evil-mode 1)
+      (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+      (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join))
 
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
+    (use-package evil-collection
+      :after evil
+      :config
+      (evil-collection-init))
 
-;; Vim jk escape
-(use-package key-chord
-  :config
-  (key-chord-mode 1)
-  (key-chord-define evil-insert-state-map  "jk" 'evil-normal-state))
+    ;; Vim jk escape
+    (use-package key-chord
+      :config
+      (key-chord-mode 1)
+      (key-chord-define evil-insert-state-map  "jk" 'evil-normal-state))
 
-(column-number-mode)
-(global-display-line-numbers-mode)
-;; (setq display-line-numbers 'relative)
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                shell-mode-hook
-                treemacs-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+    (column-number-mode)
+    (global-display-line-numbers-mode)
+    ;; (setq display-line-numbers 'relative)
+    (dolist (mode '(org-mode-hook
+                    term-mode-hook
+                    shell-mode-hook
+                    treemacs-mode-hook
+                    eshell-mode-hook))
+      (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+    ;; Make ESC quit prompts
+    (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
+    (use-package rainbow-delimiters
+      :hook (prog-mode . rainbow-delimiters-mode))
 
-;; autocomplete paired brackets
-(electric-pair-mode 1)
+    ;; autocomplete paired brackets
+    (electric-pair-mode 1)
 
 (use-package all-the-icons)
 
@@ -148,15 +145,14 @@
   :diminish projectile-mode
   :config (projectile-mode)
   :custom (projectile-completion-system 'ivy)
-  ;; :bind-keymap
-  ;; ("C-c p" . projectile-command-map)
   :init
-  (when (file-directory-p "~/projects")
-    (setq projectile-project-search-path '("~/projects")))
-  (setq projectile-switch-project-action #'magit-status))
+(when (file-directory-p "~/projects")
+      (setq projectile-project-search-path '("~/projects" "~/org")))
+;; TODO: Add back #'magit-status conditionally
+(setq projectile-switch-project-action #'projectile-dired))
 
 (crw/leader-keys
-  "p" 'projectile-command-map)
+    "p" 'projectile-command-map)
 
 (dir-locals-set-class-variables 'signal-processing-service
    '((nil . ((projectile-project-name . "signal-processing-service")))))
@@ -170,21 +166,21 @@
 (dir-locals-set-directory-class
    "~/projects/data-pipes" 'data-pipes)
 
-(use-package company
-        :after lsp-mode
-        :hook (lsp-mode . company-mode)
-        :bind (:map company-active-map
-               ("<tab>" . company-complete-selection))
-              (:map lsp-mode-map
-               ("<tab>" . company-indent-or-complete-common))
-        :custom
-        (company-minimum-prefix-length 1)
-        (company-idle-delay 0.0))
+        (use-package company
+          :after lsp-mode
+          :hook (lsp-mode . company-mode)
+          :bind (:map company-active-map
+                 ("<tab>" . company-complete-selection))
+                (:map lsp-mode-map
+                 ("<tab>" . company-indent-or-complete-common))
+          :custom
+          (company-minimum-prefix-length 1)
+          (company-idle-delay 0.0))
 
-    (setq company-backends '((company-capf company-dabbrev-code)))
+      (setq company-backends '((company-capf company-dabbrev-code)))
 
-(use-package company-box
-  :hook (company-mode . company-box-mode))
+  (use-package company-box
+    :hook (company-mode . company-box-mode))
 
 (use-package magit)
 
@@ -201,33 +197,33 @@
 (crw/leader-keys
   "t" 'term)
 
-(defun lsp-mode-setup ()
-        (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
-        (lsp-headerline-breadcrumb-mode))
+            (defun lsp-mode-setup ()
+              (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+              (lsp-headerline-breadcrumb-mode))
 
-        (use-package lsp-mode
-            :commands (lsp lsp-deferred)
-            :hook (lsp-mode . lsp-mode-setup)
-            :init
-            ;; TODO: Properly use the general leader.
-            (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-c l' 'C-l', 's-l'
-            (setq lsp-log-io t)
-            :config
-            (lsp-enable-which-key-integration t)
+              (use-package lsp-mode
+                  :commands (lsp lsp-deferred)
+                  :hook (lsp-mode . lsp-mode-setup)
+                  :init
+                  ;; TODO: Properly use the general leader.
+                  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-c l' 'C-l', 's-l'
+                  (setq lsp-log-io t)
+                  :config
+                  (lsp-enable-which-key-integration t)
+                  :custom
+                  (lsp-prefer-capf t)
+                  (lsp-auto-guess-root t)             
+                  (lsp-keep-workspace-alive nil))
+
+      (use-package lsp-ivy)
+
+          (use-package lsp-ui
+            :hook (lsp-mode . lsp-ui-mode)
             :custom
-            (lsp-prefer-capf t)
-            (lsp-auto-guess-root t)             
-            (lsp-keep-workspace-alive nil))
+            (lsp-ui-doc-position 'bottom))
 
-(use-package lsp-ivy)
-
-    (use-package lsp-ui
-      :hook (lsp-mode . lsp-ui-mode)
-      :custom
-      (lsp-ui-doc-position 'bottom))
-
-  (use-package lsp-treemacs
-    :after lsp)
+        (use-package lsp-treemacs
+          :after lsp)
 
 (load-file "~/.emacs.d/bazel/bazel.el")
 (add-to-list 'auto-mode-alist '("\\.star\\'" . bazel-starlark-mode))
@@ -258,103 +254,106 @@
 (add-hook 'c-mode-hook 'lsp)
 (add-hook 'c++-mode-hook 'lsp)
 
-;;(use-package python-mode
-  ;;  :ensure t
-  ;;  :hook (python-mode . lsp-deferred)
-  ;;  :custom
-  ;;  (dap-python-debugger 'debugpy)
-  ;;  :config
-  ;;  (require 'dap-python))
+    ;;(use-package python-mode
+    ;;  :ensure t
+    ;;  :hook (python-mode . lsp-deferred)
+    ;;  :custom
+    ;;  (dap-python-debugger 'debugpy)
+    ;;  :config
+    ;;  (require 'dap-python))
 
-(use-package lsp-python-ms
-:ensure t
-:init (setq lsp-python-ms-auto-install-server t)
-:hook (python-mode . (lambda ()
-                        (require 'lsp-python-ms)
-                        (lsp-deferred))))  ; or lsp-deferred
-  (use-package pyenv-mode
-      ;; Integrate pyenv with Python-mode
-      :init
-      (let ((pyenv-path (expand-file-name "~/.pyenv/bin")))
-      (setenv "PATH" (concat pyenv-path ":" (getenv "PATH")))
-      (add-to-list 'exec-path pyenv-path))
+  (use-package lsp-python-ms
+  :ensure t
+  :init (setq lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-python-ms)
+                          (lsp-deferred))))  ; or lsp-deferred
+    (use-package pyenv-mode
+        ;; Integrate pyenv with Python-mode
+        :init
+        (let ((pyenv-path (expand-file-name "~/.pyenv/bin")))
+        (setenv "PATH" (concat pyenv-path ":" (getenv "PATH")))
+        (add-to-list 'exec-path pyenv-path))
+        :config
+        (pyenv-mode))
+
+      (defun projectile-pyenv-mode-set ()
+        "Set pyenv version matching project name."
+        (let ((project (projectile-project-name)))
+          (if (member project (pyenv-mode-versions))
+              (pyenv-mode-set project)
+            (pyenv-mode-unset))))
+
+      (add-hook 'projectile-after-switch-project-hook 'projectile-pyenv-mode-set)
+
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.emacs-backups/"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control nil)       ; don't use versioned backups
+
+    ;; TODO: remove underline
+    (use-package org
+      :hook (org-mode . crw/org-mode-setup)
       :config
-      (pyenv-mode))
+      (setq org-ellipsis " ▾"
+            org-hide-emphasis-markers t)
 
-    (defun projectile-pyenv-mode-set ()
-      "Set pyenv version matching project name."
-      (let ((project (projectile-project-name)))
-        (if (member project (pyenv-mode-versions))
-            (pyenv-mode-set project)
-          (pyenv-mode-unset))))
+      (setq org-agenda-start-with-log-mode t)
+      ;; Filter out agenda prefix and tags.
+      (setq org-agenda-prefix-format
+          '((agenda . " %i %-12:c%?-12t% s")
+            (todo   . " ")
+            (tags   . " %i %-12:c")
+            (search . " %i %-12:c")))
+      (setq org-agenda-hide-tags-regexp ".")
+      (setq org-log-done 'time)
+      (setq org-log-into-drawer t)
 
-    (add-hook 'projectile-after-switch-project-hook 'projectile-pyenv-mode-set)
-
-(setq user-init-file "~/.emacs.d/init.el")
-(defun open-init-file ()
-  "Open the init file."
-  (interactive)
-  (find-file user-init-file))
-(crw/leader-keys
-  "i" 'open-init-file)
-
-;; TODO: remove underline
-(use-package org
-  :hook (org-mode . crw/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾"
-        org-hide-emphasis-markers t)
-
-  (setq org-agenda-start-with-log-mode t)
-  ;; Filter out agenda prefix and tags.
-  (setq org-agenda-prefix-format
-      '((agenda . " %i %-12:c%?-12t% s")
-        (todo   . " ")
-        (tags   . " %i %-12:c")
-        (search . " %i %-12:c")))
-  (setq org-agenda-hide-tags-regexp ".")
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
+      (setq org-src-preserve-indentation t)
 
 
-  ;;(setq org-capture-templates '(("t" "Todo [inbox]" entry
-  ;;                             (file+headline "~/gtd/inbox.org" "Tasks")
-  ;;                             "* TODO %i%?")))
+      ;;(setq org-capture-templates '(("t" "Todo [inbox]" entry
+      ;;                             (file+headline "~/gtd/inbox.org" "Tasks")
+      ;;                             "* TODO %i%?")))
 
-  (setq org-refile-targets '(("~/org/projects.org" :maxlevel . 3)
-                             ("~/org/someday.org" :level . 1)
-                             ("~/org/archive.org" :level . 1)
-                             ("~/org/tickler.org" :maxlevel . 2)))
+      (setq org-refile-targets '(("~/org/projects.org" :maxlevel . 3)
+                                 ("~/org/someday.org" :level . 1)
+                                 ("~/org/archive.org" :level . 1)
+                                 ("~/org/tickler.org" :maxlevel . 2)))
 
-  (setq org-agenda-files '("~/org/inbox.org"
-                           "~/org/projects.org"
-                           "~/org/tickler.org"))
-  (setq org-directory "~/org")
+      (setq org-agenda-files '("~/org/inbox.org"
+                               "~/org/projects.org"
+                               "~/org/tickler.org"))
+      (setq org-directory "~/org")
 
-  (setq org-capture-templates
-        `(("i" "Inbox" entry (file "inbox.org")
-           , (concat "* TODO %?\n"
-                     "/Entered on/ %U"))))
+      (setq org-capture-templates
+            `(("i" "Inbox" entry (file "inbox.org")
+               , (concat "* TODO %?\n"
+                         "/Entered on/ %U"))))
 
-  (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+      (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
 
-  (setq org-agenda-custom-commands 
-      '(("w" "Work-related tasks" tags-todo "@work"
-         ((org-agenda-overriding-header "Work")))
-        ("h" "Personal tasks" tags-todo "@home"
-         ((org-agenda-overriding-header "Home")))
-        ))
-  (setq org-tag-alist
-    '((:startgroup)
-       ; Put mutually exclusive tags here
-       (:endgroup)
-       ("@errand" . ?E)
-       ("@home" . ?H)
-       ("@work" . ?W)))
+      (setq org-agenda-custom-commands 
+          '(("w" "Work-related tasks" tags-todo "@work"
+             ((org-agenda-overriding-header "Work")))
+            ("h" "Personal tasks" tags-todo "@home"
+             ((org-agenda-overriding-header "Home")))
+            ))
+      (setq org-tag-alist
+        '((:startgroup)
+           ; Put mutually exclusive tags here
+           (:endgroup)
+           ("@errand" . ?E)
+           ("@home" . ?H)
+           ("@work" . ?W)))
 
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+      (advice-add 'org-refile :after 'org-save-all-org-buffers)
 
-  (crw/org-font-setup))
+      (crw/org-font-setup))
 
 ;; TODO: Some of this doesn't work?
 (defun crw/org-font-setup ()
@@ -404,18 +403,18 @@
   (use-package visual-fill-column
     :hook (org-mode . efs/org-mode-visual-fill))
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
-           '((emacs-lisp . t)
-             (python . t)))
+ (org-babel-do-load-languages
+  'org-babel-load-languages
+            '((emacs-lisp . t)
+              (python . t)))
 
-       (push '("conf-unix" . conf-unix) org-src-lang-modes)
+        (push '("conf-unix" . conf-unix) org-src-lang-modes)
 
-     (require 'org-tempo)
+      (require 'org-tempo)
 
-     (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-     (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-     (add-to-list 'org-structure-template-alist '("py" . "src python"))
+      (add-to-list 'org-structure-template-alist '("sh" . "src shell"))
+      (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+      (add-to-list 'org-structure-template-alist '("py" . "src python"))
 
 (defhydra hydra-org-tools (:timeout 4)
   "org tools"
